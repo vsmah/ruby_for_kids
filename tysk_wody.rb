5ba1820b0193999a0bcb1e6c94c390b6f3fb5238
vsmah <vlad.smahulov@gmail.com>
2023-01-15 17:25:02 +0100

    script tysk_wody

---
+++ tysk_wody.rb
@@ -0,0 +1,6 @@
+tysk_wody = 100
+puts "Tysk_wody_na_pochatku: #{tysk_wody}."
+tysk_wody = 100/2
+puts "tysk_wody_now: #{tysk_wody}!"
+tysk_wody = tysk_wody + 10
+puts "tysk_wody_v_kinci: #{tysk_wody}!"
