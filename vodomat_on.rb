87b0ea5cd3356c3fbcc7bbabdf5cb047801876eb
vsmah <vlad.smahulov@gmail.com>
2023-01-18 22:20:40 +0100

    fix vodomat

--- vodomat_on.rb
+++ vodomat_on.rb
@@ -6,4 +6,16 @@ elsif !vodomat_on
 	puts "vodomat off"
 else
 	puts "nema vody"
+end
+
+if tysk_vody > 50
+	puts "warning! tysk vody greater than 50. It is #{tysk_vody}"
+	tysk_vody = 50
+	puts "tysk vody now: #{tysk_vody}"
+elsif tysk_vody < 50
+	puts "warning! tysk vody lower than 50. It is #{tysk_vody}"
+	tysk_vody = 50
+	puts "tysk vody now: #{tysk_vody}"
+else
+	puts "tysk vody #{tysk_vody} (Yraaaaaaaaaaaaaa)"
 end 
