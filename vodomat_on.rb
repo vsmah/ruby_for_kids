5ac3fd8e7ea8c68d961ee50028ae98f90e7b31cb
vsmah <vlad.smahulov@gmail.com>
2023-01-19 23:17:20 +0100

    one more change for vodomat

--- vodomat_on.rb
+++ vodomat_on.rb
@@ -8,7 +8,7 @@ else
 	puts "nema vody"
 end
 
-if tysk_vody < 50 || tysk_vody > 50
+if tysk_vody != 50
 	puts "warning! tysk_vody ne 50! it is #{tysk_vody}."
 	tysk_vody = 50
 	puts "tysk_vody now: #{tysk_vody}"
