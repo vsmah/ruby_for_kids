d34d1b77b4ea87792b30bd67044034cabba64ca7
vsmah <vlad.smahulov@gmail.com>
2023-02-21 23:47:37 +0100

    fix hash

--- training.rb
+++ training.rb
@@ -1,7 +1,7 @@
 subjects = {
-	:andriy = "next to the river"
-	:anna = "green house"
-	:emilia = "new yavorivsk"
+	andriy: "next to the river",
+	anna: "green house",
+	emilia: "new yavorivsk"
 }
 def find_a_subject(name, subjects)
 	if subjects[name]
