2c3b1a98e5c19f0c57f7ceccd2b518fce70bfc46
vsmah <vlad.smahulov@gmail.com>
2023-03-02 19:43:52 +0100

    make some changes in monkey.rb

--- monkey.rb
+++ monkey.rb
@@ -1,8 +1,11 @@
 class Monkey
+  $home = "jungle"
   @@amount_of_monkeys = 0
-
+  
   def initialize
+    @species = species
     @@amount_of_monkeys += 1
+    puts "Create a new monkey! Now you have #{@@amount_of_monkeys}"
   end
 
   def self.amount_of_monkeys
