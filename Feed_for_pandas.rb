a4a4a48b059f68890e064090eb28c43d59752a23
vsmah <vlad.smahulov@gmail.com>
2023-03-16 19:56:29 +0100

    create code that feed pandas

---
+++ Feed_for_pandas.rb
@@ -0,0 +1,31 @@
+require "./Paint"
+
+class Feed
+  def give
+    puts "Feed is given" 
+  end
+end
+
+class FeedForPandas < Feed
+  include Paint
+  attr_reader :calories
+
+  calories_per_serving = 1000
+
+  def initialize
+  	@calories = calories_per_serving
+  end
+
+  def give
+  	puts "One feed has been given"
+  end
+
+  def analize
+  	puts "This feed has #{@calories} calories and #{color} color"
+  end
+end
+
+feed_for_hogart = FeedForPandas.new
+puts feed_for_hogart.analize
+
+
