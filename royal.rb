3f637c809385ee2f3735237073226ef695421363
vsmah <vlad.smahulov@gmail.com>
2023-03-27 17:41:12 +0200

    code without repeats

---
+++ royal.rb
@@ -0,0 +1,23 @@
+def king?(man)
+  if men == "king"
+  	puts "royal family"
+  else 
+  	puts "not royal family"
+  end
+end
+
+def queen?(woman)
+  if queen == "woman"
+	puts "royal family"
+  else
+  	puts "not royal family"
+  end
+end
+
+def royal_family?(human)
+  if human == "king" || human == "queen"
+	puts "royal family"
+  else
+	puts "not royal family"
+  end
+end
